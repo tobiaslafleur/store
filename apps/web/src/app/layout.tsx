2648b32c00662630acb993d9b67f583285f7d2d0
Tobias la Fleur <tobias.lafleur98@gmail.com>
2023-06-29 01:32:42 +0200

    build: setup pnpm and added web, server and tsconfig packages

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import '../globals.css';
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
